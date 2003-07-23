1f965a66881167dc3284254a0336f5e5b33cb37f
Petras Kudaras <moxliukas@delfi.lt>
2003-07-23 13:19:06 +0000

    Import of KUDARASP/POD2-LT-0.01_01 from CPAN.
    gitpan-cpan-distribution: POD2-LT
    gitpan-cpan-version:      0.01_01
    gitpan-cpan-path:         KUDARASP/POD2-LT-0.01_01.tar.gz
    gitpan-cpan-author:       KUDARASP
    gitpan-cpan-maturity:     developer

---
+++ LT.pm
@@ -0,0 +1,33 @@
+package POD2::LT;
+
+use 5.006;
+use strict;
+use vars qw($VERSION);
+$VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+POD2::IT - Lithuanian translation of Perl documentation
+
+=head1 SYNOPSIS
+
+  % perldoc POD2::LT::perl*;
+
+=head1 DESCRIPTION
+
+POD2::LT yra lietuviðkø Perlo dokumentacijos vertimø rinkinys.
+
+=head1 AUTHOR
+
+POD2::LT kol kas sudaro Petro Kudaro vertimai (pagalba verèiant dokumentacijà 
+bei taisant klaidas visada laukiama).
+POD2::LT paketas iðleistas Petro Kudaro <moxliukas@delfi.lt>
+
+=head1 SEE ALSO
+
+L<perl>, L<perldoc>.
+
+=cut
